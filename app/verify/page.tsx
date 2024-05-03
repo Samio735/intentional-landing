bbdb16f43721a6647bacac96f4945c5bcd388e91
Samy <s_rahim@estin.dz>
2024-05-03 22:08:47 +0100

    hello

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -2,11 +2,13 @@
 
 import { Account, Client } from "appwrite";
 import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const urlParams = new URLSearchParams(useSearchParams());
-
+  const searchParams = useSearchParams();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const urlParams = new URLSearchParams(searchParams);
+  const [verified, setVerified] = useState(false);
   useEffect(() => {
     const secret = urlParams.get("secret");
     const userId = urlParams.get("userId");
@@ -19,13 +21,30 @@ export default function Home() {
     account.updateVerification(userId, secret).then(
       function (response) {
         console.log(response);
+        setVerified(true);
       },
       function (error) {
         console.log(error);
       }
     );
-  }, []);
+  }, [urlParams]);
 
+  if (!verified) {
+    return (
+      <div className=" flex flex-col items-center">
+        <h2 className="text-xl  my-6 font-bold">intentional</h2>
+
+        <div className="h-16"></div>
+        <h1 className="text-6xl max-w-xl my-14 font-bold text-center">
+          Verifying Email
+        </h1>
+        <p className="text-center max-w-2xl">
+          Please wait while we verify your email
+        </p>
+        {/* <Subscribe /> */}
+      </div>
+    );
+  }
   return (
     <div className=" flex flex-col items-center">
       <h2 className="text-xl  my-6 font-bold">intentional</h2>

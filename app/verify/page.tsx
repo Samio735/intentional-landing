7fed2c3ecfffed00fdbd712d5b49010ac437d85b
Samy <s_rahim@estin.dz>
2024-05-03 22:12:58 +0100

    hello

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -1,62 +1,10 @@
-"use client";
+import { Suspense } from "react";
+import Verify from "../../components/Verify";
 
-import { Account, Client } from "appwrite";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-
-export default function Home() {
-  const searchParams = useSearchParams();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const urlParams = new URLSearchParams(searchParams);
-  const [verified, setVerified] = useState(false);
-  useEffect(() => {
-    const secret = urlParams.get("secret");
-    const userId = urlParams.get("userId");
-
-    const client = new Client()
-      .setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
-      .setProject("660cade3e490b591a6cb"); // Your project ID
-
-    const account = new Account(client);
-    account.updateVerification(userId, secret).then(
-      function (response) {
-        console.log(response);
-        setVerified(true);
-      },
-      function (error) {
-        console.log(error);
-      }
-    );
-  }, [urlParams]);
-
-  if (!verified) {
-    return (
-      <div className=" flex flex-col items-center">
-        <h2 className="text-xl  my-6 font-bold">intentional</h2>
-
-        <div className="h-16"></div>
-        <h1 className="text-6xl max-w-xl my-14 font-bold text-center">
-          Verifying Email
-        </h1>
-        <p className="text-center max-w-2xl">
-          Please wait while we verify your email
-        </p>
-        {/* <Subscribe /> */}
-      </div>
-    );
-  }
+export default function page() {
   return (
-    <div className=" flex flex-col items-center">
-      <h2 className="text-xl  my-6 font-bold">intentional</h2>
-
-      <div className="h-16"></div>
-      <h1 className="text-6xl max-w-xl my-14 font-bold text-center">
-        Email Verified
-      </h1>
-      <p className="text-center max-w-2xl">
-        You can go back to using the extension
-      </p>
-      {/* <Subscribe /> */}
-    </div>
+    <Suspense>
+      <Verify />
+    </Suspense>
   );
 }
